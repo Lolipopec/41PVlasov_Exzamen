ae970d9e3fdbdc6e860d73c7ee3c850463974014
Lolipopec <pacha_vlasov_95@mail.ru>
2022-04-19 10:11:42 +0300

    Лист книг

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using _41PVlasov_Exzamen.Pages;
+using _41PVlasov_Exzamen.Support_class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _41PVlasov_Exzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadPage.MainFrame = FrameMain;
+            FrameMain.Navigate(new ListBookPage());
+        }
+    }
+}
---
+++ Support class/BaseConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41PVlasov_Exzamen.Support_class
+{
+    public class BaseConnect
+    {
+        public static Entities1 BaseModel = new Entities1();
+    }
+}
